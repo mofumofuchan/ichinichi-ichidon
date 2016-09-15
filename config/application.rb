b6b5c1aff235559b2061bd29cb9a15899cb42e4a
mofumofuchan <you_oki1@yahoo.co.jp>
2016-09-16 00:13:29 +0900

    modify about web-console

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module IchinichiIchidon
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # Add my configuration
-    config.web_console.development_only = false
   end
 end
