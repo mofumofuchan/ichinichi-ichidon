6db3cc411695e1052473dafe409253df5f081694
mofumofuchan <you_oki1@yahoo.co.jp>
2016-09-15 22:59:13 +0900

    Configure test tools

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module IchinichiIchidon
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add my configuration
+    config.web_console.development_only = false
   end
 end
